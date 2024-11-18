7ee0a62a44202b532a3e0e98d46239e46de6b4c0
Amber Shipley <ashipley2@me.com>
2024-11-18 11:53:53 -0800

    Add navigation layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Bach',
@@ -13,7 +14,49 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col items-center sm:items-start font-apple">
+        <div className="w-full">
+          <header className="sticky top-0 bg-white">
+            <div className="flex items-center text-6xl my-4 pt-6 pl-36">
+              <div>🥝</div>
+              <h1>Timeless in Tulum</h1>
+            </div>
+            <ul className="flex flew-row justify-around mx-auto max-w-3xl text-xl pb-4">
+              <li>
+                <Link className="py-6" href="">
+                  home
+                </Link>
+              </li>
+              <li>
+                <Link className="py-6" href="">
+                  the vibe
+                </Link>
+              </li>
+              <li>
+                <Link className="py-6" href="">
+                  getting there
+                </Link>
+              </li>
+              <li>
+                <Link className="py-6" href="">
+                  itinerary
+                </Link>
+              </li>
+              <li>
+                <Link className="py-6" href="">
+                  packing list
+                </Link>
+              </li>
+              <li>
+                <Link className="py-6" href="">
+                  RSVP
+                </Link>
+              </li>
+            </ul>
+          </header>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,30 @@
 import Link from 'next/link';
 import CalendarDropdown from '@/components/CalendarDropdown';
-const gCalLink = '@TODO';
+
+const gMapsUrl =
+  'https://www.google.com/maps/place/Tulum,+Quintana+Roo,+Mexico/@20.1766996,-87.5052612,13509m/data=!3m2!1e3!4b1!4m6!3m5!1s0x8f4fd40310e42a4b:0xeedc0db93ff9ab7!8m2!3d20.2114185!4d-87.4653502!16s%2Fm%2F02q9y44';
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start font-apple">
-      <div className="max-w-2xl mx-4 p-6 relative bg-white/60 rounded-2xl shadow-lg backdrop-blur-md">
-        <h1>Welcum</h1>
-        <p className="font-merri">hihi</p>
-        <div className="mt-6 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-12 items-center justify-center">
-          <div className="flex items-center space-x-4">
-            <CalendarDropdown />
-            <p className="text-lg text-center md:text-left">
-              April 10-14, 2025
-            </p>
-          </div>
-
-          <div className="flex items-center space-x-4">
-            <a
-              href={gCalLink}
-              target="_blank"
-              rel="noreferrer"
-              className="card-btn"
-            >
-              📍
-            </a>
-            <p className="text-lg text-center md:text-left">Tulum</p>
-          </div>
-        </div>
+    <div className="mt-6 flex flex-col justify-center">
+      <div className="flex items-center space-x-4">
+        <CalendarDropdown />
+        <p className="text-lg text-center md:text-left">
+          April 10&ndash;14, 2025
+        </p>
       </div>
-    </main>
+
+      <div className="flex items-center space-x-4">
+        <a
+          href={gMapsUrl}
+          target="_blank"
+          rel="noreferrer"
+          className="card-btn"
+        >
+          📍
+        </a>
+        <p className="text-lg text-center md:text-left">Tulum</p>
+      </div>
+    </div>
   );
 }
