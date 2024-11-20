c637b3c5712468de589dda75a34111613f22f999
Amber Shipley <ashipley2@me.com>
2024-11-19 17:43:02 -0800

    Add custom colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
-      'apple': ['AppleGothic', 'Helvetica', 'sans-serif'],
-      'merri': ['Merriweather', 'serif'],
+      apple: ['AppleGothic', 'Helvetica', 'sans-serif'],
+      merri: ['Merriweather', 'serif'],
     },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        brownRose: '#AF8B87',
+        rose: '#C29D93',
       },
     },
   },
