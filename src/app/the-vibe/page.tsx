30203c586122a55c865d209b9fe0a3ff35e68f76
Amber Shipley <ashipley2@me.com>
2024-11-19 17:44:20 -0800

    Add content for the vibe page

---
+++ src/app/the-vibe/page.tsx
@@ -0,0 +1,31 @@
+export default function TheVibe() {
+	return (
+		<section className="max-w-4xl mx-auto bg my-10 py-8 px-14 text-2xl">
+			<p className="tracking-wide leading-relaxed text-justify">
+				Tulum is calling! I chose this magical place because it’s the perfect
+				mix of connection, adventure, and relaxation. 🌿✨ Known for its sacred
+				energy vortex, Tulum is the ideal spot to unwind, recharge, and embrace
+				a slower pace with amazing people (aka you). Whether it’s the beach, the
+				cenotes (which are magical underground water holes), or the ancient
+				ruins, it has everything I want to experience in this chapter.
+			</p>
+			<br />
+			<p className="tracking-wide leading-relaxed text-justify">
+				Whether you’re able to join for the whole trip or just part of it, I’m
+				so grateful to have each of you in my life. I want this to be a time
+				where we all feel at ease, doing whatever feels best. Some activities,
+				like a group dinner and a private boat day, will require a set
+				itinerary, so we can just do whatever feels right the rest of the time.
+				💫
+			</p>
+			<br />
+			<p className="tracking-wide leading-relaxed text-justify">
+				I chose you because you’re the ones who make life more fun, who support
+				me, and who I can truly be myself around. Together, we’ll move, explore,
+				and savor all the good moments—plus a little day drinking and dancing,
+				because, let’s be honest, it’s the best with you. 🌞✨ I’m beyond
+				grateful to share this time with each of you in such a magical place. 🌅
+			</p>
+		</section>
+	);
+}
