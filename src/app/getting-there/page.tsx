624402edf4ceb521b6e32974e4526798a4a97aca
Amber Shipley <ashipley2@me.com>
2024-11-19 17:43:54 -0800

    Add content for getting there page

---
+++ src/app/getting-there/page.tsx
@@ -0,0 +1,120 @@
+export default function GettingThere() {
+	return (
+		<section className="max-w-4xl mx-auto bg my-6 py-8 px-14 text-xl">
+			<h2 className="text-5xl">Getting There</h2>
+			<div className="tracking-wide leading-relaxed text-justify mt-4">
+				<h3 className="text-2xl font-bold mb-2">Passport Reminder 🌍</h3>
+				<p>Make sure your passport is valid for travel! </p>
+				<p className="mt-4">
+					For more information visit:
+					<a
+						href="https://travel.state.gov/content/travel/en/passports.html"
+						className="text-brownRose hover:text-rose ml-1"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						U.S. Passport Services
+					</a>
+				</p>
+				<p>
+					Check current processing times here:
+					<a
+						href="https://travel.state.gov/content/travel/en/passports/requirements/processing-times.html"
+						className="text-brownRose hover:text-rose"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Passport Processing Times
+					</a>
+				</p>
+			</div>
+			<br />
+			<div className="tracking-wide leading-relaxed text-justify">
+				<h3 className="text-2xl font-bold mb-2">Recommended Flights ✈️</h3>
+				<p>
+					I’d suggest arriving at Tulum Airport (TUY) or Cancun Airport (CUN).
+					Here are some links to recommended flights:
+				</p>
+				<p>
+					<a
+						href="https://www.google.com/flights?hl=en#flt=DEN.TUY.20250410*TUL.DEN.20250414"
+						className="text-brownRose hover:text-rose ml-4"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						- Flights from Denver, CO 🏔️
+					</a>
+				</p>
+				<p>
+					<a
+						href="https://www.google.com/flights?hl=en#flt=BLI.TUY.20250410*TUL.BLI.20250414"
+						className="text-brownRose hover:text-rose ml-4"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						- Flights from Bellingham, WA 🌲
+					</a>
+				</p>
+				<p>
+					<a
+						href="https://www.google.com/flights?hl=en#flt=TYR.TUY.20250410*TUL.TYR.20250414"
+						className="text-brownRose hover:text-rose ml-4"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						- Flights from Tyler, TX 🌵
+					</a>
+				</p>
+			</div>
+			<br />
+			<div className="tracking-wide leading-relaxed text-justify">
+				<h3 className="text-2xl font-bold mb-2">
+					From the Airport to the Airbnb 🚌
+				</h3>
+				<p>
+					If you're flying into Cancun (CUN) or Tulum (TUY), you can take the
+					ADO bus service, which operates from the airports to Tulum Centro.
+				</p>
+				<br />
+				<p>
+					Look for ADO kiosks at the airport, or use their app once you’re in
+					Mexico:
+				</p>
+				<p>
+					<a
+						href="https://apps.apple.com/mx/app/ado-boletos-de-autob%C3%BAs/id1480058029"
+						className="hover:text-rose text-brownRose ml-4"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						- ADO App for iPhone
+					</a>
+				</p>
+				<p>
+					<a
+						href="https://play.google.com/store/apps/details?id=com.mobilityado.ado&hl=en_US"
+						className="hover:text-rose text-brownRose ml-4"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						- ADO App for Android
+					</a>
+				</p>
+				<br />
+				<p>
+					Once you arrive in Tulum Centro, it’s easy to grab an Uber or local
+					taxi to the Airbnb.
+				</p>
+			</div>
+			<br />
+			<div className="tracking-wide leading-relaxed text-justify">
+				<h3 className="text-2xl font-bold mb-2">Carpooling 🚗</h3>
+				<p>
+					If it works with arrival times, I’d love to help coordinate carpooling
+					from the airport to the Airbnb! Once everyone has their flight info,
+					we can make this happen. 🌞
+				</p>
+			</div>
+		</section>
+	);
+}
